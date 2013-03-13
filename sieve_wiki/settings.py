7a71abd42ada0dff2e6d97978034dc1fe139e6d5
Thúlio Costa <thulio.costa@sieve.com.br>
2013-03-13 18:07:58 -0300

    Static paths

--- sieve_wiki/settings.py
+++ sieve_wiki/settings.py
@@ -1,4 +1,6 @@
 # Django settings for sieve_wiki project.
+import os.path
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), "..")
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,11 +63,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static_wiki")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static_wiki/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
